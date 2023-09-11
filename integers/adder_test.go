4170e0ea7d5b2eaf627fa7141761a5d6db63ee09
Juan Pablo Avilan <juan.avilan.-nd@disney.com>
2023-09-11 12:37:53 -0500

    integers module completed

---
+++ integers/adder_test.go
@@ -0,0 +1,24 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Example() {
+
+}
+func ExampleAdd() {
+	sum := Add(8, 8)
+	fmt.Println(sum)
+	//Output: 6
+}
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
